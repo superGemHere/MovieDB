effd5643218377f71230e3a8be22542853d89389
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-06 15:05:00 +0200

    project init

---
+++ moviedb/types/movie.ts
@@ -0,0 +1,14 @@
+export interface Movie {
+   id: string
+   title: string
+   year: number
+   rating: number
+   runtime: number
+   genres: string[]
+   overview: string
+   posterUrl: string
+   backdropUrl: string
+   cast: string[]
+ }
+ 
+ 
