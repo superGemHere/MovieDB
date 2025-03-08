389945d891712900fe6cac58f930c6f299d84b9a
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-09 00:28:14 +0200

    chore: create API

---
+++ moviedb/app/lib/api/movies.ts
@@ -0,0 +1,98 @@
+import axios from 'axios';
+
+const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const requester = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Get Movies
+const getMovies = async () => {
+  try {
+    const response = await requester.get("/discover/movie", {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    return [];
+  }
+};
+
+// Get Single Movie
+const getSingleMovie = async (id: number) => {
+  try {
+    const response = await requester.get(`/movie/${id}`, {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching movie details for ID ${id}:`, error);
+    return null;
+  }
+};
+
+// Get Trending Movies (Current Week)
+const getTrendingMovies = async (page?: number) => {
+  try {
+    const response = await requester.get("/trending/movie/week", {
+      params: {
+        api_key: apiKey,
+        page,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return [];
+  }
+};
+
+// Get Coming Soon Movies
+const getComingSoonMovies = async (page?: number) => {
+  try {
+    const response = await requester.get("/movie/upcoming", {
+      params: {
+        api_key: apiKey,
+        page,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching upcoming movies:", error);
+    return [];
+  }
+};
+
+// Get Top Rated Movies
+const getTopRatedMovies = async (page?: number) => {
+  try {
+    const response = await requester.get("/movie/top_rated", {
+      params: {
+        api_key: apiKey,
+        page,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching top-rated movies:", error);
+    return [];
+  }
+};
+
+
+const movieAPI = {
+  getMovies,
+  getSingleMovie,
+  getTrendingMovies,
+  getComingSoonMovies,
+  getTopRatedMovies,
+};
+
+export default movieAPI;
