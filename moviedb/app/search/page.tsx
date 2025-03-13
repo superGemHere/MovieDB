139baa35b259ee975119b2f7f6eba1b56f7cbcda
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-13 18:29:07 +0200

    feat: create search functionality and bottom pagination

--- moviedb/app/search/page.tsx
+++ moviedb/app/search/page.tsx
@@ -1,26 +1,42 @@
-import { Search } from "lucide-react"
-import MovieCard from "@/components/movie-card"
-import SearchBar from "@/components/search-bar"
-import { movies } from "@/data/movies"
-import styles from "./page.module.css"
+import { Search } from "lucide-react";
+import MovieCard from "@/components/movie-card";
+import SearchBar from "@/components/search-bar";
+import { movies } from "@/data/movies";
+import styles from "./page.module.css";
+import { Movie } from "@/types/movie";
+import movieAPI from "@/lib/api/movies";
+import PaginationControls from "@/components/PaginationControls";
+import BottomPagination from "@/components/BottomPagination";
+import type { Movies } from "@/types/PaginatedMovies";
+import { redirect } from 'next/navigation'
+import MovieSlider from "@/components/movie-slider";
 
 interface SearchPageProps {
-  searchParams: Promise<{ q?: string }>
+  searchParams: Promise<{ q?: string; page?: string }>;
 }
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
-  const query = (await searchParams).q || "";
+  const params = await searchParams;
+  const query = params.q || "";
+  const page = parseInt(params.page || "1");
 
-  // Filter movies based on search query
-  const searchResults = query
-    ? movies.filter(
-        (movie) =>
-          movie.title.toLowerCase().includes(query.toLowerCase()) ||
-          movie.overview.toLowerCase().includes(query.toLowerCase()) ||
-          movie.genres.some((genre) => genre.toLowerCase().includes(query.toLowerCase())) ||
-          movie.cast.some((actor) => actor.toLowerCase().includes(query.toLowerCase())),
-      )
-    : []
+  const movieResults = await movieAPI.findMovie(query, page);
+  const movies: Movies = Array.isArray(movieResults)
+    ? { results: [], totalPages: 0, totalResults: 0 }
+    : movieResults;
+
+    let popularMovies: Movies = {} as Movies;
+    
+    if(query){
+      if(page > movies.totalPages) {
+        redirect(`/search?q=${query}&page=${movies.totalPages}`)
+      }else if(page < 1) {
+        redirect(`/search?q=${query}&page=1`)
+      }
+    }else{
+      popularMovies = await movieAPI.getTrendingMovies(page) as Movies;
+    }
+   
 
   return (
     <main className={styles.main}>
@@ -29,38 +45,41 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
         <SearchBar />
       </div>
 
-      {/* {query ? (
-        <div className={styles.results}>
-          <h2 className={styles.resultsTitle}>
-            {searchResults.length > 0
-              ? `Found ${searchResults.length} results for "${query}"`
-              : `No results found for "${query}"`}
-          </h2>
-
-          {searchResults.length > 0 ? (
-            <div className={styles.movieGrid}>
-              {searchResults.map((movie) => (
-                // <MovieCard key={movie.id} movie={movie} />
-              ))}
+      {query
+        ? <div className={styles.results}>
+          <div className={styles.titleAndControls}>
+              <h2 className={styles.resultsTitle}>
+                {movies.results.length > 0
+                  ? `Found ${movies.totalResults} results for "${query}"`
+                  : `No results found for "${query}"`}
+              </h2>
+              <div className={styles.stats}>
+                <PaginationControls pathName="search" maxPageCount={movies.totalPages}/>
+                <span>
+                  {movies.totalPages} {`${movies.totalPages === 1 ? "Page" : "Pages"}`}
+                </span>
+              </div>
             </div>
-          ) : (
-            <div className={styles.noResults}>
-              <Search size={48} className={styles.noResultsIcon} />
-              <p>Try adjusting your search or filter to find what you're looking for.</p>
+            {movies.results.length > 0
+              ? <div className={styles.movieGrid}>
+                  {movies.results.map(movie =>
+                    <MovieCard key={movie.id} movie={movie} />
+                  )}
+                </div>
+              : <div className={styles.noResults}>
+                  <Search size={48} className={styles.noResultsIcon} />
+                  <p>
+                    Try adjusting your search or filter to find what you're
+                    looking for.
+                  </p>
+                </div>}
+          </div>
+        : popularMovies && (<div className={styles.trending}>
+            <h2 className={styles.trendingTitle}>Popular Searches</h2>
+              <MovieSlider movies={popularMovies.results} slice={20}/>
             </div>
           )}
-        </div>
-      ) : (
-        <div className={styles.trending}>
-          <h2 className={styles.trendingTitle}>Popular Searches</h2>
-          <div className={styles.movieGrid}>
-            {movies.slice(0, 12).map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
-          </div>
-        </div>
-      )} */}
+          {query && <BottomPagination totalPages={movies.totalPages} pathName="search"/>}
     </main>
-  )
+  );
 }
-
