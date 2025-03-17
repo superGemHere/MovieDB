8ed59dc4d2feb09f0717cab08ae16c6ec1fb68a8
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-17 13:38:55 +0200

    chore: change Movie interface and add Watchlist movie type

--- moviedb/types/movie.ts
+++ moviedb/types/movie.ts
@@ -1,15 +1,42 @@
 export interface Movie {
-   id: string
-   title: string
-   release_date: number
-   vote_average: string
-   runtime: number
-   genres: string[]
-   overview: string
-   poster_path: string
-   backdropUrl: string
-   cast: string[],
-   original_language: string
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
  }
  
+ export type WatchlistMovie = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+// Type for the response of the watchlist
+export type WatchlistResponse = {
+  page: number;
+  results: WatchlistMovie[];
+  total_results: number;
+  total_pages: number;
+};
  
