4f5855657421449980afa22d280836f63016b53f
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-17 13:35:33 +0200

    Update toastContext.tsx

--- moviedb/context/toastContext.tsx
+++ moviedb/context/toastContext.tsx
@@ -4,19 +4,16 @@ import React, { createContext, useContext, ReactNode } from "react";
 import { ToastContainer, toast, ToastOptions, TypeOptions } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// Define the context type
 interface ToastContextType {
    createToast: (message: string, type?: TypeOptions, options?: ToastOptions) => void;
 }
 
-// Create the context
 const ToastContext = createContext<ToastContextType | undefined>(undefined);
 
 interface ToastProviderProps {
   children: ReactNode;
 }
 
-// Create the provider component
 export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
   const createToast = (message: string, type: TypeOptions = "success", options?: ToastOptions) => {
     toast(message, { type, ...options });
@@ -25,13 +22,11 @@ export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
   return (
     <ToastContext.Provider value={{ createToast }}>
       {children}
-      {/* Global Toast Container */}
       <ToastContainer position="bottom-right" autoClose={3000} />
     </ToastContext.Provider>
   );
 };
 
-// Hook to use the context
 export const useToast = (): ToastContextType => {
   const context = useContext(ToastContext);
   if (!context) {
