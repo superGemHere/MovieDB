3acfa1f0f0c54dfb7cb07d4531e2e24d217a037c
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-26 11:55:57 +0200

    Create head.tsx

---
+++ moviedb/app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+   return (
+     <>
+       <title>MovieDB Info - Find and Discover Movies</title>
+       <meta name="title" content="MovieDB Info" />
+       <meta name="description" content="Browse and discover movies, add to your watchlist and more." />
+ 
+       <meta property="og:title" content="MovieDB Info" />
+       <meta property="og:site_name" content="MovieDB Info" />
+       <meta property="og:type" content="website" />
+       <meta property="og:url" content="https://moviedb-info.vercel.app" />
+       <meta property="og:description" content="Your ultimate movie database for discovering and exploring films from around the world." />
+     </>
+   )
+ }
