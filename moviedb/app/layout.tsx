8b9f39a9ec442b0cda44d8bfe4262f3aab476853
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-26 12:59:24 +0200

    Update layout.tsx

--- moviedb/app/layout.tsx
+++ moviedb/app/layout.tsx
@@ -7,20 +7,15 @@ import "./globals.css"
 import { AuthProvider } from "@/context/authContext"
 import { ToastProvider } from "@/context/toastContext"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "MovieDB Info - Find and Discover Movies",
   description: "Browse and discover movies, add to your watchlist and more",
-  openGraph: {
-    title: "MovieDB Info",
-    siteName: "MovieDB Info",
-    type: "website",
-    url: "https://moviedb-info.vercel.app",
-    description: "Your ultimate movie database for discovering and exploring films from around the world.",
-  },
 }
+
 export default function RootLayout({
   children,
 }: {
@@ -28,6 +23,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>MovieDB Info</title>
+        <meta property="og:title" content="MovieDB Info" />
+        <meta property="og:site_name" content="MovieDB Info" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moviedb-info.vercel.app/" />
+        <meta property="og:image" content="https://moviedb-info.vercel.app/_next/static/media/logo.3ec7b58d.webp" />
+        <meta property="og:image:alt" content="MovieDB Info Logo" />
+        <meta property="og:description" content="Explore trending, top-rated, and upcoming movies." />
+      </Head>
       <body className={inter.className}>
         <AuthProvider>
           <ToastProvider>
