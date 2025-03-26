d386f12ae840aaee374c27f44d49edaf6efa2003
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-03-26 12:49:47 +0200

    Update head.tsx

--- moviedb/app/head.tsx
+++ moviedb/app/head.tsx
@@ -8,8 +8,16 @@ export default function Head() {
        <meta property="og:title" content="MovieDB Info" />
        <meta property="og:site_name" content="MovieDB Info" />
        <meta property="og:type" content="website" />
-       <meta property="og:url" content="https://moviedb-info.vercel.app" />
-       <meta property="og:description" content="Your ultimate movie database for discovering and exploring films from around the world." />
+       <meta property="og:url" content="https://moviedb-info.vercel.app/" />
+       <meta property="og:image" content="https://moviedb-info.vercel.app/_next/static/media/logo.3ec7b58d.webp" />
+       <meta property="og:image:alt" content="MovieDB Info Logo" />
+       <meta property="og:description" content="Explore trending, top-rated, and upcoming movies." />
+ 
+       <meta name="twitter:card" content="summary_large_image" />
+       <meta name="twitter:title" content="MovieDB Info" />
+       <meta name="twitter:description" content="Explore trending, top-rated, and upcoming movies." />
+       <meta name="twitter:image" content="https://moviedb-info.vercel.app/_next/static/media/logo.3ec7b58d.webp" />
+ 
      </>
    )
  }
